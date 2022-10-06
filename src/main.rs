52f6fba15fb2ddcf01d843d4f601b9b007a855e5
el-wumbus <aidanneal4@gmail.com>
2022-10-05 21:34:46 -0400

    possible working (I love unsafe rust)

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 use redlux;
 use rodio::{OutputStream, Sink};
+use souvlaki::{MediaControlEvent, MediaPlayback, MediaControls, PlatformConfig, MediaMetadata};
 use std::{
     fs::File,
     io::{stdin, stdout, BufReader, Write},
@@ -13,6 +14,14 @@ mod select;
 
 const MAX_VOLUME: u32 = 200;
 const PRECENTAGE_CONVERSION: f32 = 100.0;
+static mut MODE: Mode = Mode::Play();
+
+enum Mode
+{
+    Play(),
+    Pause(),
+    Stop(),
+}
 
 fn main()
 {
@@ -21,7 +30,7 @@ fn main()
 
     let file_handle = File::open(file.clone())
         .expect(format!("Couldn't open file {}", file.to_string_lossy()).as_str());
-    
+
     // Do diffferent things for m4a files.
     match file.extension() // TODO: detect file type from file content https://lib.rs/crates/lofty
     {
@@ -45,15 +54,14 @@ fn main()
         _ => (),
     }
     let (_stream, stream_handle) = rodio::OutputStream::try_default().unwrap();
-            let audio = stream_handle
-                .play_once(BufReader::new(file_handle))
-                .unwrap();
-            audio_controls(audio, volume, file, disable_terminal_controls);
+    let audio = stream_handle
+        .play_once(BufReader::new(file_handle))
+        .unwrap();
+    audio_controls(audio, volume, file, disable_terminal_controls);
 }
 
 fn audio_controls(sink: Sink, mut volume: f32, file: PathBuf, no_term_controls: bool)
 {
-
     println!(
         "Playing '{}' at {}% volume",
         file.to_string_lossy(),
@@ -62,22 +70,73 @@ fn audio_controls(sink: Sink, mut volume: f32, file: PathBuf, no_term_controls: 
     sink.set_volume(volume);
     sink.play();
 
-    // TODO: parse metadata 
-    let metadata:parse::AudioMetadata = parse::AudioMetadata::from_file(file.clone());
+    let metadata: parse::AudioMetadata = parse::AudioMetadata::from_file(file.clone());
+    let (title, artist, album) = (metadata.title, metadata.artist, metadata.album);
 
-    // let mut controls = handle::handle_controls(metadata);
-    // controls
-    //     .attach(|event: MediaControlEvent| match event
-    //     {
-    //         MediaControlEvent::Pause => sink.pause(),
-    //         MediaControlEvent::Play => sink.play(),
-    //         MediaControlEvent::Quit => sink.stop(),
-    //         _ => (),
-    //     })
-    //     .unwrap();
+    #[cfg(not(target_os = "windows"))]
+    let hwnd = None;
+
+    #[cfg(target_os = "windows")]
+    let hwnd = {
+        use raw_window_handle::Win32WindowHandle;
+
+        let handle: Win32WindowHandle = unimplemented!();
+        Some(handle.hwnd)
+    };
+
+    let config = PlatformConfig {
+        dbus_name: "decator_plsplay",
+        display_name: "plsplay",
+        hwnd,
+    };
+
+    let mut controls = MediaControls::new(config).expect("Error: Unable to create media controls");
+
+    controls
+            .attach(move |event: MediaControlEvent| match event
+            {
+                MediaControlEvent::Pause => unsafe {MODE = Mode::Pause()},
+                MediaControlEvent::Play => unsafe {MODE = Mode::Play()},
+                MediaControlEvent::Quit => unsafe {MODE = Mode::Stop()},
+                _ => (),
+            })
+            .unwrap();
+            
+    controls
+    .set_metadata(MediaMetadata {
+        title: Some(&title),
+        album: Some(&album),
+        artist: Some(&artist),
+        ..Default::default()
+    })
+    .unwrap();
 
     loop
     {
+        unsafe {
+            match MODE
+            {
+                Mode::Play() =>
+                {
+                    sink.play();
+                    controls
+                        .set_playback(MediaPlayback::Playing { progress: None })
+                        .unwrap();
+                }
+                Mode::Pause() =>
+                {
+                    sink.pause();
+                    controls
+                        .set_playback(MediaPlayback::Paused { progress: None })
+                        .unwrap();
+                }
+                Mode::Stop() =>
+                {
+                    sink.stop();
+                    controls.set_playback(MediaPlayback::Stopped).unwrap();
+                }
+            }
+        }
         if sink.empty()
         {
             break;
@@ -87,10 +146,7 @@ fn audio_controls(sink: Sink, mut volume: f32, file: PathBuf, no_term_controls: 
             continue;
         }
 
-        print!(
-            "{}:: ",
-            metadata.title
-        );
+        print!("{} by {}:: ", title, artist,);
         stdout().flush().unwrap();
         let mut input: String = String::new();
         stdin().read_line(&mut input).unwrap();
@@ -113,7 +169,7 @@ fn audio_controls(sink: Sink, mut volume: f32, file: PathBuf, no_term_controls: 
                 println!("\thelp                         [display help message]");
                 println!("\texit   | quit                [Close the program]");
                 println!("\tvolume | vol <target volume> [View or adjust volume]");
-            },
+            }
 
             "volume" | "vol" =>
             {
@@ -138,7 +194,7 @@ fn audio_controls(sink: Sink, mut volume: f32, file: PathBuf, no_term_controls: 
                 {
                     println!("Volume: {}%", volume * PRECENTAGE_CONVERSION)
                 }
-            },
+            }
             _ => continue,
         }
     }
@@ -157,7 +213,7 @@ struct Opt
     volume: u32,
 
     /// Disable interactive command line controls
-    #[structopt(short,long)]
+    #[structopt(short, long)]
     disable_terminal_controls: bool,
 }
 
@@ -169,7 +225,10 @@ fn parse_args(opt: Opt) -> (PathBuf, f32, bool)
     {
         volume = MAX_VOLUME;
     }
-    return (file, ((volume / PRECENTAGE_CONVERSION as u32) as f32), opt.disable_terminal_controls);
+    return (
+        file,
+        ((volume / PRECENTAGE_CONVERSION as u32) as f32),
+        opt.disable_terminal_controls,
+    );
 }
 
-// Todo: Parse metadata
