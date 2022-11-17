6a6c868e637f6f7d01901389546917943d3943bc
Decator <decator.c@proton.me>
2022-11-17 08:26:56 -0500

    Add a powershell script

--- install.ps1
+++ install.ps1
@@ -1,1 +1,15 @@
-# I haven't made this yet, ok?
+cargo build --release
+
+if ($LASTEXITCODE)
+{
+    exit 1
+}
+
+install_path="C:\Program Files\plsplay\"
+mkdir -p "$install_path"
+cp -v target\release\plsplay.exe "$install_path"
+
+if ( $ENV:Path | select-string -SimpleMatch "$install_path" == "" ); then
+{
+    echo "'${install_path}' isn't in your PATH, add it to use plsplay."
+}
